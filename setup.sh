4d429c74e65f7f2915c779bd051ea3b2353ef9a7
Hanzhang Zeng (Roger) <hzeng@freelancer.com>
2018-01-31 11:18:55 +1100

    Added: vimrc setting and new i3 setting Updated: vimrc Updated: reminder Added: new config for compton Added: gnome-settings-daemon Updated: vimrc color scheme Added: bash git Added: bash-git-prompt and git diff vim Added: javascript syntax highlighting Removed: wall paper

--- setup.sh
+++ setup.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
 # Dpkg installation
-apt install arandr i3-blocks i3 vim git
+apt install arandr i3blocks i3 vim git feh lm-sensors compton
 
 # Vim setup
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Git Bash
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
 # Font setup
-mkdir ~/.font
+mkdir ~/.fonts
 
 git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git
-cp YosemiteSanFranciscoFont/*.ttf ~/.font
+cp YosemiteSanFranciscoFont/*.ttf ~/.fonts
 rm -rf YosemiteSanFranciscoFont
 
 git clone https://github.com/FortAwesome/Font-Awesome.git
-cp Font-Awesome/fonts/*.ttf ~/.font
+cp Font-Awesome/fonts/*.ttf ~/.fonts
 rm -rf Font-Awesome
 
+# Apply Changes
+mkdir ~/.config
+rm -rf ~/.config/i3
+cp -r i3 ~/.config/i3
+cp -r bash-git-prompt/themes ~/.bash-git-prompt/themes
+cp -r .vimrc ~/.vimrc
+cp -r .gitconfig ~/.gitconfig
 
+# Run Vim
+vim +PluginInstall +qall
